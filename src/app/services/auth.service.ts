6ab5029f963099db56c2aefedbc7a375af449e37
Mark Bulavin <cloudlet@Marks-MacBook-Pro.local>
2024-03-24 17:15:49 +0200

    changed url + changed api for sign in and create user

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
@@ -12,18 +13,18 @@ const httpOptions = {
 
 export class AuthService {
 
-  baseUrl: string = 'http://0.0.0.0:8000/';
+  baseUrl: string = environment.baseUrl;
 
   constructor(private http: HttpClient) { }
 
   login(username: string, password: string) {
-    return this.http.post(this.baseUrl + 'api/v1/token', { username, password }, httpOptions);
+    return this.http.post(this.baseUrl + 'api/v1/auth/token/', { username, password }, httpOptions);
   };
 
   register(first_name: string, last_name: string, username: string, email: string, phone_number: string, password: string, isArtist: boolean): Observable<any> {
     const profileType = isArtist ? 'Artist' : 'Listener';
     const userParams = { first_name, last_name, username, email, phone_number, password, profile_type: profileType };
-    return this.http.post(this.baseUrl + 'api/v1/create_user', userParams, httpOptions);
+    return this.http.post(this.baseUrl + 'api/v1/auth/create_user/', userParams, httpOptions);
   };
 
 }
