5435d77162eb18f8a0806b9994af84ec7a7f029a
cloudlet <mark.bularin2005@gmail.com>
2024-04-07 15:48:19 +0300

    Attempt to reproduce track in fe from be - success

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,14 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { SliderComponent } from '../slider/slider.component';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [NavbarComponent, SliderComponent],
+  imports: [NavbarComponent, SliderComponent, CommonModule],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
-export class MainComponent {
 
+export class MainComponent {
+  baseUrl: string = environment.baseUrl;
+  responseData: any = '';
+  @ViewChild('audioPlayer') audioPlayerRef!: ElementRef;
+
+  constructor(private http: HttpClient) {}
+
+  showTrack() {
+    this.http.get(this.baseUrl + 'api/v1/tracks').subscribe((res: any) => {
+      this.responseData = res;
+      console.log('RESULT', this.responseData.data);
+    });
+  }
+
+  playTrack(trackId: number) {
+    const audioPlayer: HTMLAudioElement = this.audioPlayerRef.nativeElement;
+    const trackUrl = `${this.baseUrl}api/v1/tracks/?track_id=${trackId}`;
+    audioPlayer.src = trackUrl;
+    audioPlayer.play();
+  }
 }
