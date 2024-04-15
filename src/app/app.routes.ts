fb312e1fbd054acc0662de0a883f37e3fadfda5c
cloudlet <mark.bularin2005@gmail.com>
2024-04-15 13:49:48 +0300

    created component for liked tracks + updated routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,13 +5,18 @@ import { MainComponent } from './components/main/main.component';
 import { LoginComponent } from './components/login/login.component';
 import { CreateTrackComponent } from './components/artist/create-track/create-track.component';
 import { PlaylistsComponent } from './components/playlists/playlists.component';
+import { LikedTracksComponent } from './components/liked-tracks/liked-tracks.component';
 
 
 export const routes: Routes = [
     { path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+    
     { path: 'dashboard', component: MainComponent },
+    { path: 'artist/create-track', component: CreateTrackComponent},
+
     { path: 'register', component: RegisterComponent },
     { path: 'login', component: LoginComponent },
-    { path: 'artist/create-track', component: CreateTrackComponent},
-    { path: 'playlists', component: PlaylistsComponent}
+    
+    { path: 'playlists', component: PlaylistsComponent},
+    { path: 'likes', component: LikedTracksComponent}
 ];
---
+++ src/app/components/liked-tracks/liked-tracks.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-liked-tracks',
+  standalone: true,
+  imports: [],
+  templateUrl: './liked-tracks.component.html',
+  styleUrl: './liked-tracks.component.scss'
+})
+export class LikedTracksComponent {
+
+}
