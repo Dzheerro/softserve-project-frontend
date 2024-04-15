688fa6791bab680487c708e56805ae9017c84392
cloudlet <mark.bularin2005@gmail.com>
2024-04-15 12:52:03 +0300

    created playlist component + added routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { RegisterComponent } from './components/register/register.component';
 import { MainComponent } from './components/main/main.component';
 import { LoginComponent } from './components/login/login.component';
 import { CreateTrackComponent } from './components/artist/create-track/create-track.component';
+import { PlaylistsComponent } from './components/playlists/playlists.component';
 
 
 export const routes: Routes = [
@@ -11,5 +12,6 @@ export const routes: Routes = [
     { path: 'dashboard', component: MainComponent },
     { path: 'register', component: RegisterComponent },
     { path: 'login', component: LoginComponent },
-    { path: 'artist/create-track', component: CreateTrackComponent}
+    { path: 'artist/create-track', component: CreateTrackComponent},
+    { path: 'playlists', component: PlaylistsComponent}
 ];
