ab35247d856ea8a4626b1325d6d277571987a0c0
cloudlet <mark.bularin2005@gmail.com>
2024-04-15 13:46:37 +0300

    created component for playlist

--- src/app/components/playlists/playlists.component.ts
+++ src/app/components/playlists/playlists.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../navbar/navbar.component';
 
 @Component({
   selector: 'app-playlists',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent],
   templateUrl: './playlists.component.html',
   styleUrl: './playlists.component.scss'
 })
