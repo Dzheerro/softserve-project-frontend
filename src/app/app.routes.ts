de7efe46f09783f719988531126fbccaf5e081ea
cloudlet <mark.bularin2005@gmail.com>
2024-04-22 14:55:13 +0300

    implemented create playlist form, added button which navigate into this page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,13 +6,15 @@ import { LoginComponent } from './components/authorization/login/login.component
 import { CreateTrackComponent } from './components/artist/create-track/create-track.component';
 import { PlaylistsComponent } from './components/playlists/playlists.component';
 import { LikedTracksComponent } from './components/liked-tracks/liked-tracks.component';
+import { CreatePlaylistComponent } from './components/create-playlist/create-playlist.component';
 
 
 export const routes: Routes = [
     { path: '', redirectTo: '/dashboard', pathMatch: 'full'},
     
-    { path: 'dashboard', component: MainComponent },
+    { path: 'dashboard', component: MainComponent},
     { path: 'artist/create-track', component: CreateTrackComponent},
+    { path: 'create-playlist', component: CreatePlaylistComponent},
 
     { path: 'register', component: RegisterComponent },
     { path: 'login', component: LoginComponent },
---
+++ src/app/components/create-playlist/create-playlist.component.ts
@@ -0,0 +1,45 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { environment } from '../../../environments/environment';
+import { NavbarComponent } from '../landing-page/navbar/navbar.component';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-create-playlist',
+  standalone: true,
+  imports: [CommonModule, NavbarComponent, ReactiveFormsModule],
+  templateUrl: './create-playlist.component.html',
+  styleUrl: './create-playlist.component.scss'
+})
+export class CreatePlaylistComponent {
+
+  baseUrl: string = environment.baseUrl;
+
+  form = this.formBuilder.group({
+    title: ['', Validators.required],
+    description: ''
+  });
+
+  constructor(private http: HttpClient, private formBuilder: FormBuilder, private router: Router) { }
+
+  onSubmit() {
+    if (this.form.valid) {
+      const formData = {
+        title: this.form.get('title')?.value,
+        // description: this.form.get('description')?.value
+      };
+
+      this.http.post<any>(this.baseUrl + 'api/v1/playlists/', formData).subscribe( (response) => {
+         if (response.success === true) {
+          this.router.navigate(['/playlists'])
+         }
+        },
+        (error) => {
+          console.error('Error creating playlist:', error);
+        }
+      );
+    }
+  }
+};
