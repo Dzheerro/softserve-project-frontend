e63699e152d27c38ab7a1bb6ff11ba908e15d2a9
cloudlet <mark.bularin2005@gmail.com>
2024-05-15 12:43:25 +0300

    added ability to click on track in liked tracks component which will navigate to player page

--- src/app/components/liked-tracks/liked-tracks.component.ts
+++ src/app/components/liked-tracks/liked-tracks.component.ts
@@ -3,11 +3,12 @@ import { NavbarComponent } from '../landing-page/navbar/navbar.component';
 import { FooterComponent } from '../landing-page/footer/footer.component';
 import { ActionsTrackService } from '../../services/actions/actions.service';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-liked-tracks',
   standalone: true,
-  imports: [NavbarComponent, FooterComponent, CommonModule],
+  imports: [NavbarComponent, FooterComponent, CommonModule, RouterLink],
   templateUrl: './liked-tracks.component.html',
   styleUrl: './liked-tracks.component.scss'
 })
